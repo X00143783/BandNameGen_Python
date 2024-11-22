aad9b1fb018b4c735cd00e330511de35cd942423
X00143783 <traceraine@hotmail.com>
2024-11-22 13:17:38 +0000

    Update hello message

--- BandNameGen.py
+++ BandNameGen.py
@@ -1,8 +1,6 @@
 print("This sucks")
 
 print("*****************************")
-print("*                           *")
 print("**BAND NAME GENERATOR PYTHON*")
-print("*                           *")
 print("*****************************")
 
